58d561809f77c957cd8d9a2bb6e2092747b26d97
(no author) <(no author)@531253f7-f1de-0310-85f3-ef76114adc65>
2005-04-12 07:33:06 +0000

    Added another diff line.

--- examples/upload.sh
+++ examples/upload.sh
@@ -8,6 +8,10 @@ done
 
 diff -u complex/H-W-NM-complex.pl with-embellishments/H-W-NM-embellish.pl > \
     complex-2-embellish-delta.diff
+diff -u with-embellishments/H-W-NM-embellish.pl \
+    fine-grained-site-flow/H-W-NM-fine-grained-site-flow.pl \
+    > embellish-2-fine-grained-delta.diff
 
 rsync -r -v --progress . shlomif@shell.berlios.de:/home/groups/web-cpan/htdocs/modules/HTML-Widgets-NavMenu/article/examples/
+
 
