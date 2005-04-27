8a75844a892d6a9a1ed43d98689a5a08be9d4a27
(no author) <(no author)@531253f7-f1de-0310-85f3-ef76114adc65>
2005-04-27 08:58:41 +0000

    Added syntax highlighting the codes into HTML.

--- examples/upload.sh
+++ examples/upload.sh
@@ -15,6 +15,15 @@ diff -u fine-grained-site-flow/H-W-NM-fine-grained-site-flow.pl \
     cgi-script/H-W-NM-serve.pl \
     > fine-grained-2-cgi.diff
 
+for I in complex/H-W-NM-complex.pl with-embellishments/H-W-NM-embellish.pl \
+        fine-grained-site-flow/H-W-NM-fine-grained-site-flow.pl \
+        cgi-script/H-W-NM-serve.pl \
+        simple/H-W-NM-simple.pl \
+        *.diff ; do
+        if [ "$I" -nt "$I.html" ] ; then
+            gvim-htmlize.sh "$I"
+        fi
+done
 rsync -r -v --progress . shlomif@shell.berlios.de:/home/groups/web-cpan/htdocs/modules/HTML-Widgets-NavMenu/article/examples/
 
 
