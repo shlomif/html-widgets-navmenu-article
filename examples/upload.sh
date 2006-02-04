e07fddce8dff8002ec6b771fab6935056523d5ec
(no author) <(no author)@531253f7-f1de-0310-85f3-ef76114adc65>
2006-02-04 11:04:51 +0000

    Refactored the upload.sh script.

--- examples/upload.sh
+++ examples/upload.sh
@@ -6,14 +6,27 @@ for I in $SUBDIRS ; do
     (cd "$I" && perl *.pl)
 done
 
-diff -u complex/H-W-NM-complex.pl with-embellishments/H-W-NM-embellish.pl > \
+mydiff()
+{
+    file1="$1"
+    shift
+    file2="$1"
+    shift
+    diff="$1"
+    shift
+    if [ \( "$file1" -nt "$diff" \) -o \( "$file2" -nt "$diff" \) ] ; then
+        diff -u "$file1" "$file2" > "$diff"
+    fi
+}
+
+mydiff complex/H-W-NM-complex.pl with-embellishments/H-W-NM-embellish.pl \
     complex-2-embellish-delta.diff
-diff -u with-embellishments/H-W-NM-embellish.pl \
+mydiff with-embellishments/H-W-NM-embellish.pl \
     fine-grained-site-flow/H-W-NM-fine-grained-site-flow.pl \
-    > embellish-2-fine-grained-delta.diff
-diff -u fine-grained-site-flow/H-W-NM-fine-grained-site-flow.pl \
+    embellish-2-fine-grained-delta.diff
+mydiff fine-grained-site-flow/H-W-NM-fine-grained-site-flow.pl \
     cgi-script/H-W-NM-serve.pl \
-    > fine-grained-2-cgi.diff
+    fine-grained-2-cgi.diff
 
 for I in complex/H-W-NM-complex.pl with-embellishments/H-W-NM-embellish.pl \
         fine-grained-site-flow/H-W-NM-fine-grained-site-flow.pl \
